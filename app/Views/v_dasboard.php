cfba5982a83458917f8cf4d4c69b6a9d6673edc4
tabarakallah28 <allyrangga97@gmail.com>
2023-04-10 08:54:41 +0700

    dasboard

--- app/Views/v_dasboard.php
+++ app/Views/v_dasboard.php
@@ -92,23 +92,23 @@
             </span>
           </a>
           <ul class="treeview-menu">
-            <li class="active"><a href="index.html"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
-            <li><a href="index2.html"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
+            <li class="active"><a href="index.html"><i class="fa fa-circle-o"></i> Data Buku</a></li>
+            <li><a href="index2.html"><i class="fa fa-circle-o"></i> Catagory Buku</a></li>
           </ul>
         </li>
         <li class="treeview">
           <a href="#">
             <i class="fa fa-files-o"></i>
-            <span>Layout Options</span>
+            <span>Transaksi</span>
             <span class="pull-right-container">
               <span class="label label-primary pull-right">4</span>
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="pages/layout/top-nav.html"><i class="fa fa-circle-o"></i> Top Navigation</a></li>
-            <li><a href="pages/layout/boxed.html"><i class="fa fa-circle-o"></i> Boxed</a></li>
-            <li><a href="pages/layout/fixed.html"><i class="fa fa-circle-o"></i> Fixed</a></li>
-            <li><a href="pages/layout/collapsed-sidebar.html"><i class="fa fa-circle-o"></i> Collapsed Sidebar</a></li>
+            <li><a href="pages/layout/top-nav.html"><i class="fa fa-circle-o"></i>Denda </a></li>
+            <li><a href="pages/layout/boxed.html"><i class="fa fa-circle-o"></i> Peminjaman</a></li>
+            <li><a href="pages/layout/fixed.html"><i class="fa fa-circle-o"></i> Pengembalian</a></li>
+            <li><a href="pages/layout/collapsed-sidebar.html"><i class="fa fa-circle-o"></i> Data Pengguna</a></li>
           </ul>
         </li>
     <!-- /.sidebar -->
